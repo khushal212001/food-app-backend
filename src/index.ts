0d1af1a8e55b58afbee2e72755dfc435542ea91c
Khushal <khushalgarg85886@gmail.com>
2024-04-29 16:01:54 +0530

    project backend

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import myRestrauntRoute from "./routes/MyRestrauntRoute";
+import restrauntRoute from "./routes/RestrauntRoute";
+import orderRoute from "./routes/OrderRoute";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+.then(() => console.log("connected to database"))
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
+const app = express()
+
+app.use(cors())
+
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
+app.use(express.json())
+
+
+app.get("/health", async (req: Request, res: Response) => {
+    res.send({ message: "health OK!" });
+  });  
+
+app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restraunt", myRestrauntRoute);
+app.use("/api/restraunt", restrauntRoute);
+app.use("/api/order", orderRoute);
+
+app.listen(7000, ()=>{
+    console.log("server started on localhost:7000")
+})
